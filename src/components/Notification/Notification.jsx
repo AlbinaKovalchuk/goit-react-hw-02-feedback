8e63594011dec06e64a3364e5bde784e2787c007
Albina Kovalchuk <alkadjin78@i.ua>
2023-04-04 03:25:54 +0300

    add task

---
+++ src/components/Notification/Notification.jsx
@@ -0,0 +1,14 @@
+import PropType from 'prop-types';
+import css from '../Notification/Notification.module.css';
+
+export function Notification({ message }) {
+  return (
+    <div>
+      <p className={css.message}>{message}</p>
+    </div>
+  );
+}
+
+Notification.propType = {
+  message: PropType.string.isRequired,
+};
